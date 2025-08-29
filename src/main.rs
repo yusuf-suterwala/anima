eb600706a0547ebbea33ccf28cd1a0fa7bac09c2
unknown <>
2025-08-29 13:34:36 +0530

    made a db

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use rusqlite::Connection;
 use serde::Deserialize;
 use serde_json;
 use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use ureq;
 use zstd;
 
@@ -16,7 +17,7 @@ struct AnimeEntry {
     synonyms: Vec<String>,
 }
 
-fn download_jsonl(output_filename: &Path) -> Result<(), Box<dyn Error>> {
+fn download_jsonl(output_filename: &PathBuf) -> Result<(), Box<dyn Error>> {
     let mut compressed_database = File::create_new(output_filename)?;
     let mut http_file_reader = ureq::get(
         "https://github.com/manami-project/anime-offline-database/releases/download/latest/anime-offline-database.jsonl.zst",
@@ -29,7 +30,7 @@ fn download_jsonl(output_filename: &Path) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn extract_jsonl(compressed_filename: &Path, uncompressed_filename: &Path) -> io::Result<()> {
+fn extract_jsonl(compressed_filename: &PathBuf, uncompressed_filename: &PathBuf) -> io::Result<()> {
     let compressed_file = File::open(compressed_filename)?;
     let uncompressed_file = File::create_new(uncompressed_filename)?;
 
@@ -37,22 +38,16 @@ fn extract_jsonl(compressed_filename: &Path, uncompressed_filename: &Path) -> io
 
     Ok(())
 }
-fn main() -> Result<(), Box<dyn Error>> {
-    let anidb_foldername = env::var("ANIDB_FOLDER")
-        .expect("set ANIDB_FOLDER env var")
-        .to_string();
-    let anidb_folder = Path::new(&anidb_foldername);
 
-    let compressed_filebuf = anidb_folder.join("./anime-offline-database.jsonl.zst");
-    let compressed_filepath = compressed_filebuf.as_path();
+fn setup_db(conn: &Connection) -> rusqlite::Result<()> {
+    let _ = conn.execute_batch(include_str!("./sql/anime_schema.sql"))?;
+    let _ = conn.execute_batch(include_str!("./sql/alt_title_schema.sql"))?;
 
-    let uncompressed_filebuf = anidb_folder.join("./anime-offline-database.jsonl");
-    let uncompressed_filepath = uncompressed_filebuf.as_path();
+    Ok(())
+}
 
-    download_jsonl(compressed_filepath)?;
-    extract_jsonl(compressed_filepath, uncompressed_filepath)?;
-
-    let uncompressed_file = File::open(uncompressed_filepath)?;
+fn populate_db(anime_jsonl: &PathBuf, conn: &Connection) -> Result<(), Box<dyn Error>> {
+    let uncompressed_file = File::open(anime_jsonl)?;
     let uncompressed_filebuf = BufReader::new(uncompressed_file);
 
     let mut jsonl_lines = uncompressed_filebuf.lines();
@@ -62,9 +57,29 @@ fn main() -> Result<(), Box<dyn Error>> {
     for i in jsonl_lines {
         let line = i?;
         let anime_entry: AnimeEntry = serde_json::from_str(line.as_str())?;
-        println!("{0}", anime_entry.title);
-        break;
+        conn.execute("", ())
     }
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let anidb_foldername = env::var("ANIDB_FOLDER")
+        .expect("set ANIDB_FOLDER env var")
+        .to_string();
+    let anidb_folder = Path::new(&anidb_foldername);
+
+    let anidb_temp_folder = anidb_folder.join("temp");
+
+    let compressed_jsonlpath = anidb_temp_folder.join("anime-offline-database.jsonl.zst");
+
+    let uncompressed_jsonpath = anidb_temp_folder.join("anime-offline-database.jsonl");
+
+    download_jsonl(&compressed_jsonlpath)?;
+    extract_jsonl(&compressed_jsonlpath, &uncompressed_jsonpath)?;
+
+    let conn = Connection::open(anidb_folder.join("anime.db"))?;
+
+    setup_db(&conn)?;
 
     Ok(())
 }
